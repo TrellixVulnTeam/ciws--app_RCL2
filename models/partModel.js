ab52babc30cc84a4264c08a063873a62be50449e
clifford tsungu <ctsunguc@gmail.com>
2022-04-07 15:18:34 +0100

    updated added new connection

--- models/partModel.js
+++ models/partModel.js
@@ -1,133 +1,135 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const partSchema = new Schema({
-        uiqueId:{
-        type: String,
-        },
-        stampCasting:{
-            type: String,
-        },
-        blast: {
-            type: String,
-        },
-        seatsRemoved: {
-            type: String,
-        },
-        pt10Bar:{
-            type: String,
-        },
-        TestMPI:{
-            type: String,
-        },
-        VGuides:{
-            type: String,
-        },
-        Componentsremoved:{
-            type: String,
-        },
-        waterPlatesSupplied:{
-            type: String,
-        },
-        injectorSleeveFitted:{
-            type: String,
-        },
-        combustionFace:{
-            type: String,
-        },
-        sampleTaken:{
-            type: String,
-        },
-        materialSpecification:{
-            type: String,
-        },
-        specialMaterialInstructions:{
-            type: String,
-        },
-        otherCastingMarks:{
-            type: String,
-        },
-        additionalNotes:{
-            type: String,
-        },
-        inspectionResults:{
-            type: String,
-        },
-        FIR1:{
-            type: String,
-        },
-        FIR1Comments:{
-            type: String,
-        },
-        FIR2:{
-            type: String,
-        },
-        FIR2Comments:{
-            type: String,
-        },
-        FIR3:{
-            type: String,
-        },
-        FIR3Comments:{
-            type: String,
-        },
-        FIR4:{
-            type: String,
-        },
-        FIR4Comments:{
-            type: String,
-        },
-        FIR5:{
-            type: String,
-        },
-        FIR5Comments:{
-            type: String,
-        },
-        FIR6:{
-            type: String,
-        },
-        FIR6Comments:{
-            type: String,
-        },
-        FIR7:{
-            type: String,
-        },
-        FIR7Comments:{
-            type: String,
-        },
-        FIR8:{
-            type: String,
-        },
-        FIR8Comments:{
-            type: String,
-        },
-        FIR9:{
-            type: String,
-        },
-        FIR9Comments:{
-            type: String,
-        },
-        FIR10:{
-            type: String,
-        },
-        FIR10Comments:{
-            type: String,
-        },
-        FIR11:{
-            type: String,
-        },
-        FIR11Comments:{
-            type: String,
-        },
-        FIR12:{
-            type: String,
-        },
-        FIR12Comments:{
-            type: String,
-        },},
-{ timestamps: true });
+const partSchema = new Schema(
+  {
+    uiqueId: {
+      type: String,
+    },
+    stampCasting: {
+      type: String,
+    },
+    blast: {
+      type: String,
+    },
+    seatsRemoved: {
+      type: String,
+    },
+    pt10Bar: {
+      type: String,
+    },
+    TestMPI: {
+      type: String,
+    },
+    VGuides: {
+      type: String,
+    },
+    Componentsremoved: {
+      type: String,
+    },
+    waterPlatesSupplied: {
+      type: String,
+    },
+    injectorSleeveFitted: {
+      type: String,
+    },
+    combustionFace: {
+      type: String,
+    },
+    sampleTaken: {
+      type: String,
+    },
+    materialSpecification: {
+      type: String,
+    },
+    specialMaterialInstructions: {
+      type: String,
+    },
+    otherCastingMarks: {
+      type: String,
+    },
+    additionalNotes: {
+      type: String,
+    },
+    inspectionResults: {
+      type: String,
+    },
+    FIR1: {
+      type: String,
+    },
+    FIR1Comments: {
+      type: String,
+    },
+    FIR2: {
+      type: String,
+    },
+    FIR2Comments: {
+      type: String,
+    },
+    FIR3: {
+      type: String,
+    },
+    FIR3Comments: {
+      type: String,
+    },
+    FIR4: {
+      type: String,
+    },
+    FIR4Comments: {
+      type: String,
+    },
+    FIR5: {
+      type: String,
+    },
+    FIR5Comments: {
+      type: String,
+    },
+    FIR6: {
+      type: String,
+    },
+    FIR6Comments: {
+      type: String,
+    },
+    FIR7: {
+      type: String,
+    },
+    FIR7Comments: {
+      type: String,
+    },
+    FIR8: {
+      type: String,
+    },
+    FIR8Comments: {
+      type: String,
+    },
+    FIR9: {
+      type: String,
+    },
+    FIR9Comments: {
+      type: String,
+    },
+    FIR10: {
+      type: String,
+    },
+    FIR10Comments: {
+      type: String,
+    },
+    FIR11: {
+      type: String,
+    },
+    FIR11Comments: {
+      type: String,
+    },
+    FIR12: {
+      type: String,
+    },
+    FIR12Comments: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-const Part = mongoose.model('Part', partSchema);
+const Part = mongoose.model("Part", partSchema);
 
 module.exports = Part;
-
